6589aca7c6139aebf11f1af39d4e1dfc16ca5030
PejmanNik <pejman.nikram@sievo.com>
2023-07-20 19:01:01 +0300

    ignore error in loading images fix #17

--- packages/react/src/components/Image/index.tsx
+++ packages/react/src/components/Image/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
 import useLayoutSuspension from 'components/useLayoutSuspension';
+import log from 'core/log';
 import {ComponentProps, ReactEventHandler, useCallback} from 'react';
 
 type EventHandler = ReactEventHandler<HTMLImageElement>;
 
-function Image({onLoad, ...props}: ComponentProps<'img'>) {
+
+function Image({onLoad, onError, ...props}: ComponentProps<'img'>) {
   const {release} = useLayoutSuspension(`IMG_${props.src}`);
 
   const handleLoad = useCallback<EventHandler>(
@@ -14,8 +17,16 @@ function Image({onLoad, ...props}: ComponentProps<'img'>) {
     [onLoad, release],
   );
 
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <img {...props} onLoad={handleLoad} />;
+  const handleError = useCallback<EventHandler>(
+    event => {
+      onError?.(event);
+      release();
+      log.error(`Image load failed: ${props.src}`);
+    },
+    [onError, props.src, release],
+  );
+
+  return <img {...props} onLoad={handleLoad} onError={handleError} />;
 }
 
 export default Image;
